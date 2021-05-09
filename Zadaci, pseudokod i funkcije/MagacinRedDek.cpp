29c76727e1e08ca287e17d150bf0d1eb2c39a944
toskeee14 <andonnet@gmail.com>
2021-05-09 16:50:14 +0200

    oktobar 2020 stack

--- Zadaci, pseudokod i funkcije/MagacinRedDek.cpp
+++ Zadaci, pseudokod i funkcije/MagacinRedDek.cpp
@@ -81,6 +81,44 @@ char* Infix2Postfix(char* expr) // j duzina jbg
 }
 
 /*
+Oktobar 2020
+Napisati funkciju koja odredjuje vrednost izraza zadatog u postfiks notaciji (npr. "2 3 + 1 0 * 5 /").
+Definisati pomocnu strukturu koja se koristi za odredjivanje izraza i napisati funkcije
+dodavanja i brisanja iz pomocne strukture. Izraz je dat kao niz karaktera tako da su svi brojevi
+i operatori razdvojeni tacno jednim blanko znakom.
+*/
+
+int calc(char op, int op1, int op2) {
+	switch (op) {
+		case '+': return op1+op2;
+		case '-': return op1-op2;
+		case '*': return op1*op2;
+		case '/': return op1/op2;
+	}
+}
+
+calculatePostfix(char* expr) {
+	Stack opStack();
+	int buf;
+	for (int i=0; i<strlen(expr); i+=2) {
+		buf = 0;
+		if (expr[i] >= '0' && expr[i] <= '9') {
+			while (expr[i] >= '0' && expr[i] <= '9') {
+				buf *= 10;
+				buf += expr[i] - '0';
+			}
+			opStack.push(buf);
+		}
+		else if (expr[i] == '+' || expr[i] == '-' || expr[i] == '*' || expr[i] == '/') {
+			int op2 = opStack.pop();
+			int op1 = opStack.pop();
+			opStack.push(calc(expr[i], op1, op2));			
+		}
+	}
+	return opStack.pop();
+}
+
+/*
 2017 kol 1
 Napisati funkciju bool isCorrect(char* inStr), koja utvrduje da li je aritmeticki izraz,
 zadat kao ulazni niz karaktera, ispravan sa stanovista zatvaranja zagrada. U izrazu se mogu pojaviti
